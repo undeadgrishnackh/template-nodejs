48d64eda124f3b1f847c928ff0f63e7273078358
undeadgrishnackh <undeadgrishnackh@gmail.com>
2021-08-12 09:40:36 +0200

    feat: new CyberDojo test and commit

---
+++ scripts/test_and_commit.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+jest_test () { jest  --coverage --verbose; }
+get_date() { date +"%Y%m%d.%H%M"; }
+get_run() { git log --oneline | grep -c "TestRun"; }
+
+build_git_commit_message() {
+  MESSAGE=""
+  if [[ $1 -eq 0 ]]; then
+    MESSAGE="✅ GREEN TestRun ⏰ $(get_date) 🆔 $(get_run)"
+  else
+    MESSAGE="❌ RED TestRun ⏰ $(get_date) 🆔 $(get_run)"
+  fi
+
+  echo "$MESSAGE"
+}
+
+git_commit() {
+  echo git add --all
+  echo git commit -m "\"$1\""
+}
+
+jest_test
+MESSAGE=$(build_git_commit_message $?)
+git_commit "$MESSAGE"
